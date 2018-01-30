30c8114e2f895815d0e677949b47531aa2e336be
aligobabaee <ali.golbabaeel22@gmail.com>
2018-01-31 00:03:33 +0330

    add homeController and view Home with thair style

---
+++ Hotel/script/home.js
@@ -0,0 +1,27 @@
+﻿//  var a = document.getElementById("login_mask");
+function closeAll() {
+    document.getElementById("login_mask").classList.remove("show");
+    document.getElementById("login_mask").classList.add("hide");
+    //  a.classList.add("hide");
+}
+function showLog() {
+    // alert("jdfsfldf");
+    document.getElementById("sign_mask").classList.add("hide");
+    document.getElementById("login_mask").classList.remove("hide");
+    document.getElementById("login_mask").classList.add("show");
+    document.getElementById("sign_mask").classList.remove("show");
+
+
+}
+/////////////Login////////////////////////
+function showsign() {
+    document.getElementById("login_mask").classList.remove("show");
+    document.getElementById("login_mask").classList.add("hide");
+    document.getElementById("sign_mask").classList.remove("hide");
+    document.getElementById("sign_mask").classList.add("show");
+}
+function closeAlll() {
+    //  a.classList.add("hide");
+    document.getElementById("sign_mask").classList.remove("show");
+    document.getElementById("sign_mask").classList.add("hide");
+}
