5c02522b224d81b05582875dc49e8257eac4c8dc
aligobabaee <ali.golbabaeel22@gmail.com>
2018-01-31 10:54:16 +0330

    add database class
    add tables

---
+++ Hotel/Models/Hotel_DB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Hotel.Models
+{
+    public class Hotel_DB:DbContext
+    {
+        public Hotel_DB()
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+    }
+}
